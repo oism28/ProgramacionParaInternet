73e39d1be45ba8aa48fe9b41a0206cc58173e1af
oism28 <oism28@hotmail.com>
2025-02-22 16:24:13 -0600

    CambiosMenores

--- Actividad4/scriptPortafolio.js
+++ Actividad4/scriptPortafolio.js
@@ -9,25 +9,41 @@ function validarNombre(e){
         error.style.display = 'block';
         error.innerHTML += '<li>Completar nombre</li>';
         console.log('Por favor agregue el nombre');
+        e.preventDefault();
     }else{
         error.style.display = 'none';       
     }
 }
+
+
 function validarCorreo(e){
     if (correo == '' || correo.value == null){
         error.style.display = 'block';
         error.innerHTML += '<li>Completar correo</li>';
         console.log('Por favor agregue el correo');
+        e.preventDefault();
     }else{
         error.style.display = 'none';       
     }
 }
-function validarTeléfono(e){
-    if (numeroTel == '' || numeroTel.value == null){
+
+/*Función para validar el teléfono */
+function validarTelefono(e){
+    if (numeroTel == '' || numeroTel.value == null || numeroTel.length !== 10){
         error.style.display = 'block';
         error.innerHTML += '<li>Completar Teléfono</li>';
         console.log('Por favor agregue el Teléfono');
+        e.preventDefault();
     }else{
         error.style.display = 'none';       
     }
 }
+
+function validarForm(e){
+    error.innerHTML = '';
+    validarCorreo(e);
+    validarTelefono(e);
+    validarNombre(e);
+}
+
+formularios.addEventListener('submit', validarForm);
