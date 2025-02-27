113cceee786172aa1cfc9a1473fb53425a25023e
oism28 <oism28@hotmail.com>
2025-02-27 15:30:49 -0600

    Update animaciones.js

--- Actividad7/animaciones.js
+++ Actividad7/animaciones.js
@@ -6,3 +6,68 @@ $(document).ready(function(){
         });
     });
 });
+
+$(document).ready(function(){
+    // $(selector).animate({parametros}, velocidad, callback);
+    $('#boton').on('click', function(){
+        $('#caja').animate({
+            width: '300px',
+            opacity: '0.2'
+        });
+    });
+});
+
+$(document).ready(function(){
+    // $(selector).animate({parametros}, velocidad, callback);
+    $('#boton').on('click', function(){
+        $('#caja').animate({
+            width: '300px',
+            opacity: '0.2'},
+            3000);
+        });
+    });
+
+$(document).ready(function(){
+    // $(selector).animate({parametros}, velocidad, callback);
+    $('#boton').on('click', function(){
+        $('#caja').animate({
+            width: '300px',
+            opacity: '0.2'
+        },3000, function(){
+            alert ('Fin de la animación');
+        });
+    });
+});
+
+$(document).ready(function(){
+    // $(selector).animate({parametros}, velocidad, callback);
+    $('#boton').on('click', function(){
+        $('#caja').animate({
+            width: '+=50px'
+        },1000, function(){
+            //alert ('Fin de la animación');
+        });
+    });
+});
+
+$(document).ready(function(){
+    // $(selector).animate({parametros}, velocidad, callback);
+    $('#boton').on('click', function(){
+        $('#caja').animate({
+            margin: '+=20px'
+        },1000, function(){
+        //alert ('Fin de la animación');
+        });
+    });
+});
+
+$(document).ready(function(){
+    // $(selector).animate({parametros}, velocidad, callback);
+    $('#boton').on('click', function(){
+        $('#caja').animate({
+            marginLeft: '+=50px'
+        },500, function(){
+        //alert ('Fin de la animación');
+        });
+    });
+});
