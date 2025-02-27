7ab4cdf6ecc7895d9946ddf3fae6f80af8a14c23
oism28 <oism28@hotmail.com>
2025-02-27 14:33:39 -0600

    Ejercicio3.2 terminado
    Se termino el ejercicio 3.2 y se agregaron los js a cada respectivo ejercicio

--- Actividad7/eventos.js
+++ Actividad7/eventos.js
@@ -1,5 +1,33 @@
-$(document).ready(function(){
+/*$(document).ready(function(){
     var boton = $('#boton');
     boton.on('click', function(){document.body.style.background = '#000'   //javascript
+    });
+});
+
+$(document).ready(function(){
+    var boton = $('#boton');
+    boton.on('mouseenter', function(){
+        document.body.style.background = '#000'  //javascript
+    });
+    boton.on('mouseleave', function(){
+        document.body.style.background = '#fff'  //javascript
+    });
+});
+
+$(document).ready(function(){
+    var boton = $('#boton');
+    boton.on('click', function(){
+        alert('Saludos');
+    });
+    $('#desactivar').on('click', function(){
+        boton.off('click');
+        alert ('ha quedado desactivado el botón "Ejecutar"')
+    });
+});*/
+
+$(document).ready(function(){
+    $('a').on('click', function(e){
+        e.preventDefault();
+        alert('link desactivado');
     });
 });
